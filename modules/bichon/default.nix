bf22ac4b35d0cc055c5fc367120eb18e2051e41a
Morgan Helton <mhelton@gmail.com>
2026-01-31 11:39:03 -0600

    feat: add bichon application resources

--- modules/bichon/default.nix
+++ modules/bichon/default.nix
@@ -1,8 +1,14 @@
-{ ... }:
+{ lib, ... }:
 {
   applications.bichon = {
     namespace = "bichon";
     createNamespace = true;
+
+    resources = {
+      deployments.bichon = builtins.head (lib.kube.fromYAML (builtins.readFile ./deployment.yaml));
+      services.bichon = builtins.head (lib.kube.fromYAML (builtins.readFile ./service.yaml));
+      ingresses.bichon = builtins.head (lib.kube.fromYAML (builtins.readFile ./ingress.yaml));
+    };
 
     yamls = [
       (builtins.readFile ./bichon-secret.sops.yaml)
